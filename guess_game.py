c00eb737c646370de06ed82bacff31c56db77e2f
Anthony New <AnthonyNew2001@proton.me>
2025-06-04 20:43:25 -0700

    Update guess_game.py
    I fixed it, now work

--- guess_game.py
+++ guess_game.py
@@ -1,59 +1,54 @@
 import random
 
-#number = random.randint(1, 10)
 print("Welcome to Anthony's Guessing Game BRU")
-print("choose a difficulty")
+print("Choose a difficulty")
 print("1: 1-10")
 print("2: 1-30")
 print("3: 1-99")
 
-diff = int(input("enter a diffculty between 1-3"))
+diff = int(input("Enter a difficulty between 1-3: "))
 
 if diff == 1:
-    number = random.randint(1, 10)
+    max_num = 10
 elif diff == 2:
-    number = random.randint(1, 30)
+    max_num = 30
 elif diff == 3:
-    number = random.randint(1, 992)
+    max_num = 99
 else:
-    print ("invalid option")
-    number = random.randint(1, 10)
+    print("Invalid option, defaulting to 1-10")
+    max_num = 10
 
-guess = int(input("Guess a number between 1 and max "))
+number = random.randint(1, max_num)
 
-# give the user a hint.
-if guess < number:
-    print("Too low!")
-elif guess > number:
-    print("Too high!")
+guess = int(input(f"Guess a number between 1 and {max_num}: "))
 
-
-## Check if the guess is correct
+# First guess
 if guess == number:
     print("You got it!")
 else:
+    if guess < number:
+        print("Too low!")
+    else:
+        print("Too high!")
 
-#if the user is wrong, let them try again
-    #tell the user they have 2 more tries
-    print("2 more tries !")
-    guess = int(input("Try again! Guess a number between 1 and 10: "))
+    print("2 more tries!")
+
+    # Second guess
+    guess = int(input(f"Try again! Guess a number between 1 and {max_num}: "))
     if guess == number:
         print("You got it!")
     else:
-
-        # give the user a hint.
         if guess < number:
             print("Too low!")
-        elif guess > number:
+        else:
             print("Too high!")
 
-        #tell the user they have 1 more tries
-        print("last try!!!! !")
-        guess = int(input("Try again! Guess a number between 1 and 10: "))
+        print("Last try!!!")
+
+        # Third guess
+        guess = int(input(f"Try again! Guess a number between 1 and {max_num}: "))
         if guess == number:
             print("You got it!")
         else:
-
-            print("Sorry, the number was", number)
+            print(f"Sorry, the number was {number}")
             print("Better luck next time!")
-
